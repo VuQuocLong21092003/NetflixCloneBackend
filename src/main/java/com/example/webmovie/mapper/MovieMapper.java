14da30915e4bc5518c06a2dbd52be83083479e80
ASUS <vulong25082033@gmail.com>
2025-03-07 13:59:12 +0700

    Mapper dữ diệu cho Movie

--- src/main/java/com/example/webmovie/mapper/MovieMapper.java
+++ src/main/java/com/example/webmovie/mapper/MovieMapper.java
@@ -1,6 +1,7 @@
 package com.example.webmovie.mapper;
 
 import com.example.webmovie.Model.Movie;
+import com.example.webmovie.dto.request.MovieRequest;
 import com.example.webmovie.dto.response.MovieSearchResponse;
 import org.mapstruct.Mapper;
 
@@ -11,4 +12,6 @@ public interface MovieMapper {
     MovieSearchResponse toMovieSearchResponse(Movie movie);
 
     List<MovieSearchResponse> toMovieSearchResponse(List<Movie> movies);
+
+    Movie toMovie(MovieRequest movieRequest);
 }
