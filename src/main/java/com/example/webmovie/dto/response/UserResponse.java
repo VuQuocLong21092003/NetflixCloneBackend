2a585a5ae25a2072ebbef94a4e8c79a08b4d6636
ASUS <vulong25082033@gmail.com>
2025-03-10 23:00:17 +0700

    Thêm 2 trường cho UserResponse

--- src/main/java/com/example/webmovie/dto/response/UserResponse.java
+++ src/main/java/com/example/webmovie/dto/response/UserResponse.java
@@ -1,16 +1,22 @@
 package com.example.webmovie.dto.response;
 
+import com.example.webmovie.untils.Gender;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.*;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserResponse implements Serializable {
     Long id;
     private String fullName;
+    private LocalDate dateOfBirth;
+    private Gender gender;
+
 }
