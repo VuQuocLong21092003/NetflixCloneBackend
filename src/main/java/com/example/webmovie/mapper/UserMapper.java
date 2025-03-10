d30ba7000bf2722303a100969f65cb1a25f2e29c
ASUS <vulong25082033@gmail.com>
2025-03-10 23:01:30 +0700

    Tạo mapping để bắt các trường

--- src/main/java/com/example/webmovie/mapper/UserMapper.java
+++ src/main/java/com/example/webmovie/mapper/UserMapper.java
@@ -5,13 +5,15 @@ import com.example.webmovie.dto.request.UpdateUseRequest;
 import com.example.webmovie.dto.request.UserRequest;
 import com.example.webmovie.dto.response.UserResponse;
 import org.mapstruct.Mapper;
-
+import org.mapstruct.Mapping;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toUser(UserRequest userRequest);
 
+    @Mapping(source = "dateOfBirth", target = "dateOfBirth")
+    @Mapping(source = "gender", target = "gender")
     UserResponse toUserReponse(User user);
 
     List<UserResponse> toUserResponseList(List<User> users);
